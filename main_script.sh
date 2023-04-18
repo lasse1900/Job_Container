a194d9204266aa7bc2a619c81761678e200e0441
Lauri Kyttälä <lauri.kyttala@etteplan.com>
2023-04-18 12:18:18 +0300

    added basic functionality to make two (2) sample requests to RapidAPI, save responses to the output.json file and sort by DATE to sorted.json

---
+++ main_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# remove previous requests response JSON files 
+rm -f input_1.json
+rm -f input_2.json
+
+python request_jobs.py 0 input_1.json django tampere
+sleep 2
+python request_jobs.py 0 input_2.json "python developer" tampere
+
+echo "Hi, I'm sleeping for 5 seconds..." 
+sleep 5
+
+python append_json_files.py
+
+echo "Hi, I'm sleeping for 5 seconds..." 
+sleep 5
+
+python sort_json.py
